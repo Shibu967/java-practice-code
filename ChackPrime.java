47626156bd9d3339c54133e8fe195fb13312f115
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-30 23:28:14 +0530

    Check Prime

---
+++ ChackPrime.java
@@ -0,0 +1,17 @@
+public class ChackPrime {
+    public static void main(String[] args) {
+        int num = 3;
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+            if (count == 2) {
+                System.out.println("prime");
+            }
+
+        }
+
+    }
+
+}
