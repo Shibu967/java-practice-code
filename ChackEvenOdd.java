7bf67e7b7ef59617b206db55f97977ab83589e38
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-30 23:26:49 +0530

    Chack Even Odd

---
+++ ChackEvenOdd.java
@@ -0,0 +1,12 @@
+public class ChackEvenOdd {
+    public static void main(String[] args) {
+        int num = 20;
+        for (int i = 0; i <= num; i++) {
+            if (i % 2 == 0) {
+                System.out.println("Even " + i);
+            } /// else {
+              // System.out.println("odd " + i);
+              // }
+        }
+    }
+}
