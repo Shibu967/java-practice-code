e5fccb162a21338506f6c34835517743c0311a16
Shibu967 <105337612+Shibu967@users.noreply.github.com>
2022-10-30 23:28:55 +0530

    find Large Number

---
+++ findLargeNumber.java
@@ -0,0 +1,13 @@
+public class findLargeNumber {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+        if (a > b) {
+            System.out.println(a + " is Large Number");
+        } else {
+            System.out.println(b + " is small number");
+        }
+
+    }
+}
